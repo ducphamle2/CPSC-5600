a90106b87f8bdc6f0cc9f9d0ee0923f9b1dff8a8
ducphamle2 <ducphamle212@gmail.com>
2023-01-17 23:23:20 -0800

    finished sequential version of the prefix sum

--- Session7.Heap/hw2.cpp
+++ Session7.Heap/hw2.cpp
@@ -56,9 +56,21 @@ public:
         return value(node);
     }
 
+    void printSumHeap()
+    {
+        int i = 0;
+        while (i < n - 1 + n)
+        {
+            cout << "|" << value(i) << "|";
+            i++;
+        }
+        cout << endl;
+    }
+
     void prefixSums(Data *data)
     {
-        throw std::invalid_argument("prefix sums error");
+        calPrefix(data, 0, 0);
+        // throw std::invalid_argument("prefix sums error");
     }
 
 private:
@@ -81,6 +93,22 @@ private:
         interior->at(i) = value(left(i)) + value(right(i));
     }
 
+    void calPrefix(Data *data, int i, int sumPrior)
+    {
+        if (isLeaf(i))
+        {
+            // we will traverse through the whole heap. If its leaf then original array index is i - (n - 1) aka minus interior index
+            // size of heap is n - 1 + n, and leaf starts at n - 1, so prefix at 0 is leaf at n - 1 and so on
+            data->at(i - (n - 1)) = sumPrior + value(i);
+            // cout << sumPrior + value(i) << endl;
+        }
+        else
+        {
+            calPrefix(data, left(i), sumPrior);
+            calPrefix(data, right(i), sumPrior + value(left(i)));
+        }
+    }
+
     int parent(int childIndex)
     {
         return (childIndex - 1) / 2;
@@ -97,31 +125,62 @@ private:
     }
 };
 
-const int N = 1 << 26; // FIXME must be power of 2 for now
+const int N = 16; // FIXME must be power of 2 for now
+
+Data genTestData()
+{
+    Data data(N, 1);
+    data[0] = 2;
+    data[1] = 7;
+    data[2] = 1;
+    data[3] = 1;
+    data[4] = 4;
+    data[5] = 2;
+    data[6] = 9;
+    data[7] = 8;
+    data[8] = 7;
+    data[9] = 1;
+    data[10] = 4;
+    data[11] = 1;
+    data[12] = 2;
+    data[13] = 2;
+    data[14] = 3;
+    data[15] = 0;
+    return data;
+}
 
 int main()
 {
     Data data(N, 1); // put a 1 in each element of the data array
-    data[0] = 10;
+    // data[0] = 10;
+    data = genTestData();
     Data prefix(N, 1);
 
     // start timer
-    auto start = chrono::steady_clock::now();
+    // auto start = chrono::steady_clock::now();
 
     SumHeap heap(&data);
+    heap.printSumHeap();
     heap.prefixSums(&prefix);
 
-    // stop timer
-    auto end = chrono::steady_clock::now();
-    auto elpased = chrono::duration<double, milli>(end - start).count();
+    // print prefix after prefix sum
+    for (int i = 0; i < N; i++)
+    {
+        cout << "|" << prefix.at(i) << "|";
+    }
+    cout << endl;
 
-    int check = 10;
-    for (int elem : prefix)
-        if (elem != check++)
-        {
-            cout << "FAILED RESULT at " << check - 1;
-            break;
-        }
-    cout << "in " << elpased << "ms" << endl;
+    // // stop timer
+    // auto end = chrono::steady_clock::now();
+    // auto elpased = chrono::duration<double, milli>(end - start).count();
+
+    // int check = 10;
+    // for (int elem : prefix)
+    //     if (elem != check++)
+    //     {
+    //         cout << "FAILED RESULT at " << check - 1;
+    //         break;
+    //     }
+    // cout << "in " << elpased << "ms" << endl;
     return 0;
 }
