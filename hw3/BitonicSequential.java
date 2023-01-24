ebf9f882f06a8515f6917fcf7d6f1e240c9f81c9
ducphamle2 <ducphamle212@gmail.com>
2023-01-24 13:48:20 -0800

    addded non-recursive sequential bitonic sort & comments

--- hw3/BitonicSequential.java
+++ hw3/BitonicSequential.java
@@ -9,8 +9,8 @@
  * @versioon 24-Jan-2020
  */
 public class BitonicSequential {
-    public static final int N = 1 << 4; // size of the final sorted array (power of two)
-    public static final int TIME_ALLOWED = 10; // seconds
+    public static final int N = 1 << 5; // FIXME: size of the final sorted array (power of two). Change to 1 << 22
+    public static final int TIME_ALLOWED = 1; // FIXME: change to 10 seconds
 
     /**
      * Main entry for HW3 assignment.
@@ -20,29 +20,36 @@ public class BitonicSequential {
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
         int work = 0;
-        // while (System.currentTimeMillis() < start + TIME_ALLOWED * 1000) {
-        double[][] data = new double[4][];
-        for (int section = 0; section < data.length; section++) {
-            data[section] = RandomArrayGenerator.getArray(N / 4);
-            StageOne.process(data[section]); // Just sorts it
-        }
-        for (int section = 0; section < data.length; section++) {
-            for (int i = 0; i < data[section].length; i++) {
-                System.out.println("data element with section " + section + ": " + data[section][i]);
+        while (System.currentTimeMillis() < start + TIME_ALLOWED * 1000) {
+            double[][] data = new double[4][];
+            for (int section = 0; section < data.length; section++) {
+                data[section] = RandomArrayGenerator.getArray(N / 4);
+                StageOne.process(data[section]); // Just sorts it
             }
+
+            // for (int section = 0; section < data.length; section++) {
+            // for (int i = 0; i < data[section].length; i++) {
+            // System.out.println("data element with section " + section + ": " +
+            // data[section][i]);
+            // }
+            // }
+            // Note that BitonicStage assumes both its input arrays are sorted
+            // increasing. It then inverts its second input to form a true bitonic
+            // sequence from the concatenation of the first input with the inverted
+            // second input.
+            BitonicStage bitonic = new BitonicStage();
+            double[] penult1 = bitonic.process(data[0], data[1]);
+            double[] penult2 = bitonic.process(data[2], data[3]);
+            double[] ult = bitonic.process(penult1, penult2);
+            if (!RandomArrayGenerator.isSorted(ult) || N != ult.length) {
+                System.out.println("failed");
+                System.exit(1);
+            }
+            work++;
+            System.out.println("start with time allowed: " + (start + TIME_ALLOWED * 1000));
+            System.out.println("current time millis: " + System.currentTimeMillis());
+            System.out.println("should break: " + ((start + TIME_ALLOWED * 1000) < System.currentTimeMillis()));
         }
-        // Note that BitonicStage assumes both its input arrays are sorted
-        // increasing. It then inverts its second input to form a true bitonic
-        // sequence from the concatenation of the first input with the inverted
-        // second input.
-        BitonicStage bitonic = new BitonicStage();
-        double[] penult1 = bitonic.process(data[0], data[1]);
-        double[] penult2 = bitonic.process(data[2], data[3]);
-        double[] ult = bitonic.process(penult1, penult2);
-        if (!RandomArrayGenerator.isSorted(ult) || N != ult.length)
-            System.out.println("failed");
-        work++;
-        // }
         System.out.println("sorted " + work + " arrays (each: " + N + " doubles) in "
                 + TIME_ALLOWED + " seconds");
     }
--- hw3/BitonicStage.java
+++ hw3/BitonicStage.java
@@ -7,50 +7,72 @@ public class BitonicStage {
 
     private double[] bitonicSeq;
 
+    /**
+     * flip - reverse the position of an array data, meaning that the array elements
+     * start at n - 1 to 0
+     * 
+     * @param data - the original array data
+     * @return reversed array data
+     */
     private double[] flip(double[] data) {
         double[] flippedData = new double[data.length];
         int index = 0;
         for (int i = data.length - 1; i >= 0; i--) {
-            flippedData[index] = data[i];
+            flippedData[index] = data[i]; // first index of new array = last index of original array
             index++;
         }
         return flippedData;
     }
 
+    /**
+     * concat - concat two given arrays into one, where the first args is first,
+     * second args is second
+     * 
+     * @param firstHalf  - the first half of the concat, also an array
+     * @param secondHalf - the second half of the concat, also an array
+     * @return - the concat array combining the two arrays given
+     */
     private double[] concat(double[] firstHalf, double[] secondHalf) {
         int firstLength = firstHalf.length;
         int secondLength = secondHalf.length;
         double[] concatArray = new double[firstLength + secondLength];
-        System.arraycopy(firstHalf, 0, concatArray, 0, firstLength);
-        System.arraycopy(secondHalf, 0, concatArray, firstLength, secondLength);
+        System.arraycopy(firstHalf, 0, concatArray, 0, firstLength); // copy the first array into the new concatArray
+        System.arraycopy(secondHalf, 0, concatArray, firstLength, secondLength); // append the 2nd array into the new
+                                                                                 // concatArray with new length
         return concatArray;
     }
 
+    /**
+     * swap - swap two array elements of the instance variable bitonicSeq
+     * 
+     * @param i - first index to swap
+     * @param j - second index to swap
+     */
+    private void swap(int i, int j) {
+        double temp = bitonicSeq[i];
+        bitonicSeq[i] = bitonicSeq[j];
+        bitonicSeq[j] = temp;
+    }
+
     private void bitonicMerge(int start, int n, Direction direction) {
-        System.out.println("start: " + start + " end: " + (start + n / 2));
+        // System.out.println("start: " + start + " end: " + (start + n / 2));
         for (int i = start; i < start + n / 2; i++) {
-            System.out.println("bitonic merge chunk: " + bitonicSeq[i]);
+            // System.out.println("bitonic merge chunk: " + bitonicSeq[i]);
         }
         if (direction == Direction.UP) {
             for (int i = start; i < start + n / 2; i++) {
-                if (bitonicSeq[i] > bitonicSeq[i + n / 2]) {
+                if (bitonicSeq[i] > bitonicSeq[i + n / 2])
                     // swap
-                    double temp = bitonicSeq[i];
-                    bitonicSeq[i] = bitonicSeq[i + n / 2];
-                    bitonicSeq[i + n / 2] = temp;
-                }
+                    swap(i, i + n / 2);
             }
         } else if (direction == Direction.DOWN) {
             for (int i = start; i < start + n / 2; i++) {
-                if (bitonicSeq[i] < bitonicSeq[i + n / 2]) {
+                if (bitonicSeq[i] < bitonicSeq[i + n / 2])
                     // swap
-                    double temp = bitonicSeq[i];
-                    bitonicSeq[i] = bitonicSeq[i + n / 2];
-                    bitonicSeq[i + n / 2] = temp;
-                }
+                    swap(i, i + n / 2);
             }
         }
-        System.out.println("end chunk");
+        // System.out.println("end chunk");
     }
 
     private void bitonicSort(int start, int n, Direction direction) {
@@ -61,22 +83,133 @@ public class BitonicStage {
         }
     }
 
+    private boolean isInArray(int[] array, int value) {
+        for (int i = 0; i < array.length; i++) {
+            if (value == array[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void bitonicSortNoRecursion(int start, int n) {
+        // UP - i holds the dot
+        // DOWN - i + j holds the dot
+        Direction dot = Direction.UP;
+        int moveICount = 0;
+        int movePlusJCount = 0;
+
+        for (int k = 1; k < n; k *= 2) {
+            // System.out.println("k: " + k);
+            for (int j = k; j >= 1; j /= 2) {
+                // when j picks the next value, we come back to step 2, reset the dot to i &
+                // move counts
+                dot = Direction.UP;
+                moveICount = 0;
+                movePlusJCount = 0;
+                // size of datum is j+1 because we only need to store in total j datums before i
+                // reaches j. After that, we can rotate j to keep storing new j index
+                int[] datumPlusJ = new int[j + 1];
+                int datumIndex = 0;
+                for (int i = 0; i < n; i++) {
+
+                    int plusJ = i + j;
+                    // System.out.println("index i: " + i + " index i + j: " + plusJ);
+                    if (plusJ > n - 1) {
+                        // if plusJ cant move then we break the loop, j picks the new value
+                        break;
+                    }
+                    // System.out.println("move i count: " + moveICount);
+                    // System.out.println("move plus j count: " + movePlusJCount);
+                    // based on bitonic dance, if total move of i >= k*2 => switch the dot
+                    if (moveICount >= k * 2) {
+                        dot = Direction.DOWN;
+                        moveICount = 0;
+                    }
+                    if (movePlusJCount >= k * 2) {
+                        dot = Direction.UP;
+                        movePlusJCount = 0;
+                    }
+                    // System.out.println("dot after checking move count: " + dot);
+                    // we increment the move of i and i+j based on the dot status to measure the
+                    // current move count.
+                    switch (dot) {
+                        case UP:
+                            moveICount++;
+                            break;
+                        case DOWN:
+                            movePlusJCount++;
+                        default:
+                            break;
+                    }
+                    // if the current index i has been compared before aka in the datum list, then
+                    // we move left instead of comparing
+                    if (i != 0 && isInArray(datumPlusJ, i)) {
+                        continue;
+                    }
+                    // store the datum that we have compared so that we can search the datum in the
+                    // next iteration
+                    datumPlusJ[datumIndex] = plusJ;
+                    datumIndex++;
+                    // rotate datum list so we can store the datum list efficiently & fast lookup
+                    if (datumIndex == datumPlusJ.length)
+                        datumIndex = 0;
+
+                    // System.out.println("bitonic elements to compare: " + bitonicSeq[i] + " and :"
+                    // + bitonicSeq[plusJ]);
+
+                    // now we compare based on the dots
+                    switch (dot) {
+                        // i holds the dot
+                        case UP:
+                            if (bitonicSeq[i] > bitonicSeq[plusJ]) {
+                                // System.out.println("swap up");
+                                swap(i, plusJ);
+                            }
+                            break;
+                        // i + j holds the dot
+                        case DOWN:
+                            if (bitonicSeq[plusJ] > bitonicSeq[i]) {
+                                // System.out.println("swap down");
+                                swap(i, plusJ);
+                            }
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * flip second array and concat with 1st to create a new array, which is a
+     * bitonic sequence
+     * 
+     * @param firstHalf  - sorted array in asc
+     * @param secondHalf - sorted array in asc
+     * @return sorted concat new array
+     */
     public double[] process(double[] firstHalf, double[] secondHalf) {
+        // we flip the 2nd half because we assume both arrays are sorted in ASC order,
+        // after flipping the secondHalf array is sorted in DESC.
         secondHalf = flip(secondHalf);
+        // We flip 2nd one then concat to make the new array a bitonic sequence.
         double[] concatArray = concat(firstHalf, secondHalf);
 
-        // initiate bitonic sequence based on the inputs
+        // initiate bitonic sequence based on the inputs so that we dont have to pass in
+        // the bitonic seq into the bitonic sort method
         bitonicSeq = concatArray;
 
-        for (int i = 0; i < bitonicSeq.length; i++) {
-            System.out.println("bitonic sequence before sorting: " + bitonicSeq[i]);
-        }
+        // for (int i = 0; i < bitonicSeq.length; i++) {
+        // System.out.println("bitonic sequence before sorting: " + bitonicSeq[i]);
+        // }
 
-        bitonicSort(0, bitonicSeq.length, Direction.UP);
+        bitonicSortNoRecursion(0, bitonicSeq.length);
+        // bitonicSort(0, bitonicSeq.length, Direction.UP);
 
-        for (int i = 0; i < bitonicSeq.length; i++) {
-            System.out.println("bitonic sequence after sorting: " + bitonicSeq[i]);
-        }
+        // for (int i = 0; i < bitonicSeq.length; i++) {
+        // System.out.println("bitonic sequence after sorting: " + bitonicSeq[i]);
+        // }
         System.out.println("end process");
         return bitonicSeq;
     }
